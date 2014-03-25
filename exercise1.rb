6b9ea1d6dc125207f3ecf0c0b1146400912d1bd4
HypeJonze <kmerisanu@yahoo.ca>
2014-03-25 17:52:24 -0400

    Update exercise1.rb

--- exercise1.rb
+++ exercise1.rb
@@ -2,11 +2,11 @@ puts "What's your name?"
 
 name = gets.chomp
 
-puts "Hi #{name}!"
+puts "Hi #{name}, welcome!"
 
 puts "What's your age?"
 
 age = gets.chomp.to_i
 year = 2014 - age
 
-puts "You were born on year #{year}"
+puts "You were born in the year #{year}"
